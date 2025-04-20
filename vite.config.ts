0702efb11c1c8660f6624b457e9536cef4980655
Maria Parinova <maryia.parynava@gmail.com>
2025-04-20 19:58:29 +0200

    fix: deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import type { UserConfig } from 'vite';
+import { defineConfig, UserConfig } from 'vite';
 
-export default {
-  build: {
-    target: 'es2022',
-  },
-} satisfies UserConfig;
+export default defineConfig(({ mode }) => {
+  const config: UserConfig = {
+    build: {
+      target: 'es2022',
+    },
+  };
+
+  if (mode === 'production') {
+    config.base = './';
+  }
+
+  return config;
+});
